423c6fc7838d69f573780c9222aaa38cbdaa11f1
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-27 01:15:09 +0100

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""This module defines a basic caching system."""
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    defines a basic caching system.
+    """
+    def put(self, key, item):
+        """
+        implements the put method.
+        inserts key,value pairs in a dictionary.
+        """
+        if (key or item) is None:
+            pass
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        implements the get method.
+        fetches a value from the cache by it's key.
+        return None if the value of the key does not exist.
+        """
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,1 @@
+This directory contains practice codes on caching system design algorithms.
