13f0f1978e166b4d988fe73d1ac9d7c8143177b0
Chisom Ibekwe <lucasbolt700@gmail.com>
2023-07-30 17:07:07 +0100

    task

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -16,13 +16,12 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """implements a put system.
         """
-        if key is None or item is None:
-            return
-        self.cache_data[key] = item
-        if len(self.cache_data) > super().MAX_ITEMS:
-            d_item = list(self.cache_data.keys())[0]
-            del (self.cache_data[d_item])
-            print(f'Discard: {d_item}')
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+            if len(self.cache_data) > super().MAX_ITEMS:
+                d_item = list(self.cache_data.keys())[0]
+                del (self.cache_data[d_item])
+                print(f'Discard: {d_item}')
 
     def get(self, key):
         """
